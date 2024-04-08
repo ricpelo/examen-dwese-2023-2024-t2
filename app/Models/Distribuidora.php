79d314b1596e870c7bd0174154ba12374acca242
Ricardo Pérez <ricpelo@gmail.com>
2024-04-08 20:13:54 +0200

    Después de las migraciones

---
+++ app/Models/Distribuidora.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Distribuidora extends Model
+{
+    use HasFactory;
+}
